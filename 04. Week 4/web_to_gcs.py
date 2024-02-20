4b45994d137b37188a627c4c81dceb049c3ad73d
voduyquoc <voduyquockt@gmail.com>
2024-02-20 14:21:18 +0100

    Update

--- 04. Week 4/web_to_gcs.py
+++ 04. Week 4/web_to_gcs.py
@@ -58,6 +58,34 @@ def web_to_gcs(year, service):
         upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
         print(f"GCS: {service}/{file_name}")
 
+def web_to_gcs_new(year, service):
+    for i in range(12):
+        
+        # sets the month part of the file_name string
+        month = '0'+str(i+1)
+        month = month[-2:]
+
+        # csv file_name
+        file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
+
+        # download it using requests via a pandas df
+        request_url = f"{init_url}{service}/{file_name}"
+        r = requests.get(request_url)
+        open(file_name, 'wb').write(r.content)
+        print(f"Local: {file_name}")
+
+        # read it back into a parquet file
+        df = pd.read_csv(file_name, compression='gzip')
+        change_types = {"PUlocationID": "Int64", "DOlocationID": "Int64", "SR_Flag": "Int64"}
+        df = df.astype(change_types)
+        file_name = file_name.replace('.csv.gz', '.parquet')
+        df.to_parquet(file_name, engine='pyarrow')
+        print(f"Parquet: {file_name}")
+
+        # upload it to gcs 
+        upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
+        print(f"GCS: {service}/{file_name}")
+
 web_to_gcs('2019', 'fhv')
 # web_to_gcs('2019', 'green')
 # web_to_gcs('2020', 'green')
